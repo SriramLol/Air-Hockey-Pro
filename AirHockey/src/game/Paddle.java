f0e6a82dec5ffb3864854551136bb8aa7f678d5a
SriramLol <69857603+SriramLol@users.noreply.github.com>
2025-03-09 13:19:19 -0400

    Update Paddle.java

--- AirHockey/src/game/Paddle.java
+++ AirHockey/src/game/Paddle.java
@@ -3,21 +3,26 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Paddle extends Polygon{
+public class Paddle extends Polygon {
 	
 	private static final int MOVE_SPEED = 5;
 	private static final int ROTATE_SPEED = 15;
 	private Color fillColor = Color.WHITE;
 	private Color borderColor = Color.BLUE;
+	private double velocityX = 0;
+	private double velocityY = 0;
+	private boolean isMoving = false;
+	private int gameWidth, gameHeight; 
 
-	public Paddle(Point[] inShape, Point inPosition, double inRotation) {
+	public Paddle(Point[] inShape, Point inPosition, double inRotation, int gameWidth, int gameHeight) {
 		super(inShape, inPosition, inRotation);
-			
+		this.gameWidth = gameWidth;
+		this.gameHeight = gameHeight;
 	}
 	
 	public static Point[] createRect(int width, int height) {
 		Point[] shape = new Point[4];
-		shape[0] = new Point(0,0);
+		shape[0] = new Point(0, 0);
 		shape[1] = new Point(width, 0);
 		shape[2] = new Point(width, height);
 		shape[3] = new Point(0, height);
@@ -25,31 +30,61 @@ public class Paddle extends Polygon{
 	}
 	
 	public void moveUp() {
-		position.setY(position.getY() - MOVE_SPEED);
+		if (position.getY() > 0) { 
+			position.setY(position.getY() - MOVE_SPEED);
+			velocityY = -MOVE_SPEED;
+			isMoving = true;
+		} else {
+			stopMoving();
+		}
 	}
-	
+
 	public void moveDown() {
-		position.setY(position.getY() + MOVE_SPEED);
+		if (position.getY() < gameHeight - getHeight()) {
+			position.setY(position.getY() + MOVE_SPEED);
+			velocityY = MOVE_SPEED;
+			isMoving = true;
+		} else {
+			stopMoving();
+		}
 	}
-	
+
 	public void moveRight() {
-		position.setX(position.getX() + MOVE_SPEED);
+		if (position.getX() < gameWidth - getWidth()) {
+			position.setX(position.getX() + MOVE_SPEED);
+			velocityX = MOVE_SPEED;
+			isMoving = true;
+		} else {
+			stopMoving();
+		}
 	}
-	
+
 	public void moveLeft() {
-		position.setX(position.getX() - MOVE_SPEED);
+		if (position.getX() > 0) { 
+			position.setX(position.getX() - MOVE_SPEED);
+			velocityX = -MOVE_SPEED;
+			isMoving = true;
+		} else {
+			stopMoving();
+		}
 	}
-	
+
+	public void stopMoving() {
+		velocityX = 0;
+		velocityY = 0;
+		isMoving = false;
+	}
+
 	public void rotateClockwise() {
 		rotate(ROTATE_SPEED);
 	}
-	
+
 	public void draw(Graphics brush) {
 		Point[] points = getPoints();
 		int[] xToColor = new int[points.length];
 		int[] yToColor = new int[points.length];
 		
-		for(int i = 0; i < points.length; i++) {
+		for (int i = 0; i < points.length; i++) {
 			xToColor[i] = (int) points[i].getX();
 			yToColor[i] = (int) points[i].getY();
 		}
@@ -60,7 +95,29 @@ public class Paddle extends Polygon{
 		brush.setColor(borderColor);
 		brush.drawPolygon(xToColor, yToColor, points.length);
 	}
-	
+
+	// New methods for tracking movement
+	public boolean isMoving() {
+		return isMoving;
+	}
+
+	public double getVelocityX() {
+		return velocityX;
+	}
+
+	public double getVelocityY() {
+		return velocityY;
+	}
+
+	public int getWidth() {
+		return (int) (getPoints()[2].getX() - getPoints()[0].getX());
+	}
+
+	public int getHeight() {
+		return (int) (getPoints()[2].getY() - getPoints()[0].getY());
+	}
+
+	// Keeps your original PaddleEffect class intact
 	public class PaddleEffect {
 		private int duration;
 		private int currentTime;
@@ -86,16 +143,12 @@ public class Paddle extends Polygon{
 		}
 		
 		public void update() {
-			if(on) {
+			if (on) {
 				currentTime += 100;
-				if(currentTime >= duration) {
+				if (currentTime >= duration) {
 					turnOff();
 				}
 			}
 		}
-		
-		
 	}
-	
-
 }
